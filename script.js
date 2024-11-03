6f1d218895196ac546b10483d757136e8257ff82
Dakodah Spurlin <dcspurlin@gmail.com>
2024-11-03 01:40:02 -0600

    Created vaiables to add data from input into table.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 console.log('Hello World!')
 function submitClick(event){
     event.preventDefault(event)
-    console.log("testing")
+    const firstName = document.querySelector("#firstNameInput").value
+    const lastName = document.querySelector("#lastNameInput").value
+    const idNumber = document.querySelector("#idInput").value
+    const jobTitle = document.querySelector("#titleInput").value
+    const annualSalary = document.querySelector("#annualSalaryInput").value
+    console.log(firstName, lastName, idNumber, jobTitle, annualSalary)
+    let employeeTable = document.querySelector("#employeeList tbody")
+    employeeTable.innerHTML += `
+    <tr>
+        <td> ${firstName} </td>
+        <td> ${lastName} </td>
+        <td> ${idNumber} </td>
+        <td> ${jobTitle} </td>
+        <td> ${annualSalary} </td>
+    `
 }
